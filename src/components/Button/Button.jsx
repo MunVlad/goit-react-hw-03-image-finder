2ab6cf668c112a413a0c79a44ee580abf08b26cb
MunVlad <mvv2006@ukr.net>
2023-05-14 19:18:06 +0300

    finder

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import css from './Button.module.css';
+import PropTypes from 'prop-types';
+
+export default function Button ({onClick}){
+    return (
+        <button type="button" onClick={onClic} className = {css.button}>Load more</button>
+    )
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func.isRequired
+}
+
